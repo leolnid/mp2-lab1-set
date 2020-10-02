89f6d5857dda436e9868aef6d92ef00a304d301d
Leo Dukov <leonid.dyukov@gmail.com>
2020-10-02 22:30:26 +0300

    Translate and remove comments

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -1,4 +1,4 @@
-﻿// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
 //
 // sample_prime_numbers.cpp - Copyright (c) Гергель В.П. 20.08.2000
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
@@ -6,37 +6,35 @@
 // Тестирование битового поля и множества
 
 #include <iomanip>
+using namespace std;
 
-// #define USE_SET // Использовать класс TSet,
+#define USE_SET // Использовать класс TSet,
                 // закоментировать, чтобы использовать битовое поле
 
 #ifndef USE_SET // Использовать класс TBitField
 
 #include "tbitfield.h"
 
+
 int main()
 {
   int n, m, k, count;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки битового поля" << endl;
-  cout << "             Решето Эратосфена" << endl;
-  cout << "Введите верхнюю границу целых значений - ";
+  cout << "Testing program support of bit set" << endl;
+  cout << "       Sieve of Eratosthenes      " << endl;
+  cout << "Please, input upper limit:  ";
   cin  >> n;
   TBitField s(n + 1);
-  // заполнение множества
   for (m = 2; m <= n; m++)
     s.SetBit(m);
-  // проверка до sqrt(n) и удаление кратных
   for (m = 2; m * m <= n; m++)
-    // если m в s, удаление кратных
     if (s.GetBit(m))
       for (k = 2 * m; k <= n; k += m)
         if (s.GetBit(k))
           s.ClrBit(k);
-  // оставшиеся в s элементы - простые числа
-  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
-  cout << endl << "Печать простых чисел" << endl;
+
+  cout << endl << "Set of undivided number: " << s << endl;
+  cout << endl << "Prime numbers: ";
   count = 0;
   k = 1;
   for (m = 2; m <= n; m++)
@@ -47,8 +45,7 @@ int main()
       if (k++ % 10 == 0)
         cout << endl;
     }
-  cout << endl;
-  cout << "В первых " << n << " числах " << count << " простых" << endl;
+  cout << endl << "In first " << n << " elements count " << count << " prime number" << endl;
 }
 #else
 
@@ -58,25 +55,21 @@ int main()
 {
   int n, m, k, count;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки множества" << endl;
-  cout << "              Решето Эратосфена" << endl;
-  cout << "Введите верхнюю границу целых значений - ";
+  cout << "Testing program support of bit set" << endl;
+  cout << "       Sieve of Eratosthenes      " << endl;
+  cout << "Please, input upper limit:  ";
   cin  >> n;
   TSet s(n + 1);
-  // заполнение множества
   for (m = 2; m <= n; m++)
     s.InsElem(m);
-  // проверка до sqrt(n) и удаление кратных
   for (m = 2; m * m <= n; m++)
-    // если м в s, удаление кратных
     if (s.IsMember(m))
       for (k = 2 * m; k <= n; k += m)
        if (s.IsMember(k))
          s.DelElem(k);
-  // оставшиеся в s элементы - простые числа
-  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
-  cout << endl << "Печать простых чисел" << endl;
+
+  cout << endl << "Set of undivided number: " << endl << s << endl;
+  cout << endl << "Prime numbers: " << endl;
   count = 0;
   k = 1;
   for (m = 2; m <= n; m++)
@@ -88,7 +81,7 @@ int main()
         cout << endl;
     }
   cout << endl;
-  cout << "В первых " << n << " числах " << count << " простых" << endl;
+  cout << "In first " << n << " elements count " << count << " prime number" << endl;
 }
 
 #endif
